048001219a4b0e4d16e88f2229e9d8abee25be3e
蔡金锋 <jeff.cai@99bill.com>
2022-02-28 19:45:18 +0800

    fix: 补充重庆县

--- tests/shared/dataTest.ts
+++ tests/shared/dataTest.ts
@@ -1,38 +1,47 @@
 function dataTest(data) {
-  it('检查数据', () => {
-    expect(data.length).toBe(3218);
-    expect(data[0]).toEqual({ code: '110000', name: '北京市' });
-    expect(data[data.length - 1]).toEqual({ code: '820000', name: '澳门特别行政区' });
+  it("检查数据", () => {
+    expect(data.length).toBe(3219);
+    expect(data[0]).toEqual({ code: "110000", name: "北京市" });
+    expect(data[data.length - 1]).toEqual({
+      code: "820000",
+      name: "澳门特别行政区",
+    });
     // 包含省
-    expect(data).toContainEqual({ code: '110000', name: '北京市' });
+    expect(data).toContainEqual({ code: "110000", name: "北京市" });
     // 包含市
-    expect(data).toContainEqual({ code: '110100', name: '北京市' });
+    expect(data).toContainEqual({ code: "110100", name: "北京市" });
     // 包含区
-    expect(data).toContainEqual({ code: '110101', name: '东城区' });
+    expect(data).toContainEqual({ code: "110101", name: "东城区" });
   });
 
-  it('直辖市', () => {
-    expect(data).toContainEqual({ code: '110100', name: '北京市' });
-    expect(data).toContainEqual({ code: '120100', name: '天津市' });
-    expect(data).toContainEqual({ code: '310100', name: '上海市' });
-    expect(data).toContainEqual({ code: '500100', name: '重庆市' });
+  it("直辖市", () => {
+    expect(data).toContainEqual({ code: "110100", name: "北京市" });
+    expect(data).toContainEqual({ code: "120100", name: "天津市" });
+    expect(data).toContainEqual({ code: "310100", name: "上海市" });
+    expect(data).toContainEqual({ code: "500100", name: "重庆市" });
   });
 
-  it('部分省补充县级行政区划', () => {
-    expect(data).toContainEqual({ code: '469000', name: '省直辖县级行政区划' });
-    expect(data).toContainEqual({ code: '659000', name: '自治区直辖县级行政区划' });
+  it("部分省补充县级行政区划", () => {
+    expect(data).toContainEqual({ code: "469000", name: "省直辖县级行政区划" });
+    expect(data).toContainEqual({
+      code: "659000",
+      name: "自治区直辖县级行政区划",
+    });
   });
 
-  it('海南省三沙市补充区级数据', () => {
-    expect(data).toContainEqual({ code: '460321', name: '西沙群岛' });
-    expect(data).toContainEqual({ code: '460322', name: '南沙群岛' });
-    expect(data).toContainEqual({ code: '460323', name: '中沙群岛的岛礁及其海域' });
+  it("海南省三沙市补充区级数据", () => {
+    expect(data).toContainEqual({ code: "460321", name: "西沙群岛" });
+    expect(data).toContainEqual({ code: "460322", name: "南沙群岛" });
+    expect(data).toContainEqual({
+      code: "460323",
+      name: "中沙群岛的岛礁及其海域",
+    });
   });
 
-  it('港澳台', () => {
-    expect(data).toContainEqual({ code: '710000', name: '台湾省' });
-    expect(data).toContainEqual({ code: '810000', name: '香港特别行政区' });
-    expect(data).toContainEqual({ code: '820000', name: '澳门特别行政区' });
+  it("港澳台", () => {
+    expect(data).toContainEqual({ code: "710000", name: "台湾省" });
+    expect(data).toContainEqual({ code: "810000", name: "香港特别行政区" });
+    expect(data).toContainEqual({ code: "820000", name: "澳门特别行政区" });
   });
 }
 
--- tests/shared/pcFormTest.ts
+++ tests/shared/pcFormTest.ts
@@ -1,22 +1,47 @@
 function pcFormTest(data) {
-  it('检查数据', () => {
+  it("检查数据", () => {
     expect(data[0].children.length).toBe(1);
-    expect(data[0].children[0]).toEqual({ value: '110100', label: '北京市' });
+    expect(data[0].children[0]).toEqual({ value: "110100", label: "北京市" });
   });
 
-  it('直辖市', () => {
-    expect(data).toContainEqual({ value: '110000', label: '北京市', children: [{ value: '110100', label: '北京市' }] });
-    expect(data).toContainEqual({ value: '120000', label: '天津市', children: [{ value: '120100', label: '天津市' }] });
-    expect(data).toContainEqual({ value: '310000', label: '上海市', children: [{ value: '310100', label: '上海市' }] });
-    expect(data).toContainEqual({ value: '500000', label: '重庆市', children: [{ value: '500100', label: '重庆市' }] });
+  it("直辖市", () => {
+    expect(data).toContainEqual({
+      value: "110000",
+      label: "北京市",
+      children: [{ value: "110100", label: "北京市" }],
+    });
+    expect(data).toContainEqual({
+      value: "120000",
+      label: "天津市",
+      children: [{ value: "120100", label: "天津市" }],
+    });
+    expect(data).toContainEqual({
+      value: "310000",
+      label: "上海市",
+      children: [{ value: "310100", label: "上海市" }],
+    });
+    expect(data).toContainEqual({
+      value: "500000",
+      label: "重庆市",
+      children: [
+        { value: "500100", label: "重庆市" },
+        { value: "500200", label: "县" },
+      ],
+    });
   });
 
-  it('部分省补充县级行政区划', () => {
-    const hn = data.find(item => item.value === '460000');
-    expect(hn.children).toContainEqual({ value: '469000', label: '省直辖县级行政区划' });
+  it("部分省补充县级行政区划", () => {
+    const hn = data.find((item) => item.value === "460000");
+    expect(hn.children).toContainEqual({
+      value: "469000",
+      label: "省直辖县级行政区划",
+    });
 
-    const xj = data.find(item => item.value === '650000');
-    expect(xj.children).toContainEqual({ value: '659000', label: '自治区直辖县级行政区划' });
+    const xj = data.find((item) => item.value === "650000");
+    expect(xj.children).toContainEqual({
+      value: "659000",
+      label: "自治区直辖县级行政区划",
+    });
   });
 }
 
